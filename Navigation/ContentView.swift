94dd1e3c2e2024fbd78a6876c446c68b1c434e32
Vianna Moshtaghi <vianna.mosh@gmail.com>
2025-08-07 10:51:20 -0700

    fixed home bar, now home thing doesnt show up on top of home screeeeeen

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -38,6 +38,8 @@ struct ContentView: View {
             }
             .padding()
             .navigationTitle(("Home"))
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
         }
     }
 }
